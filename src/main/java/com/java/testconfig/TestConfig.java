d11f6a06cb1f755c2e1d8e6e3bb3a8304d575707
fellipySaldanha <fellipy.saldanha@gmail.com>
2019-12-23 22:03:09 -0200

    State and City

--- src/main/java/com/java/testconfig/TestConfig.java
+++ src/main/java/com/java/testconfig/TestConfig.java
@@ -7,17 +7,28 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.java.domain.Category;
+import com.java.domain.City;
 import com.java.domain.Product;
+import com.java.domain.State;
 import com.java.repositories.CategoryRepository;
+import com.java.repositories.CityRepository;
 import com.java.repositories.ProductRepository;
+import com.java.repositories.StateRepository;
 
 @Configuration
 public class TestConfig implements CommandLineRunner{
+	
 	@Autowired
 	CategoryRepository categoryRepository;
 	
 	@Autowired
 	ProductRepository productRepository;
+	
+	@Autowired
+	StateRepository stateRepository;
+	
+	@Autowired
+	CityRepository cityRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -32,8 +43,20 @@ public class TestConfig implements CommandLineRunner{
 
 		p1.getCategories().addAll(Arrays.asList(cat1));
 		p1.getCategories().addAll(Arrays.asList(cat2));
-		
+				
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
 		productRepository.saveAll(Arrays.asList(p1,p2));
+		
+		State state1 = new State(null, "Minas Gerais");
+		State state2 = new State(null, "São Paulo");
+		
+		City city1 = new City(null, "Uberlândia", state1);
+		City city2 = new City(null, "São Paulo", state2);
+		
+		state1.getCities().addAll(Arrays.asList(city1));
+		state2.getCities().addAll(Arrays.asList(city2));
+		
+		stateRepository.saveAll(Arrays.asList(state1,state2));
+		cityRepository.saveAll(Arrays.asList(city1,city2));
 	}
 }
