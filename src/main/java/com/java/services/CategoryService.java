4895e885856db42acb1e6dae616b75324a9515f2
fellipySaldanha <fellipy.saldanha@gmail.com>
2019-12-27 16:34:55 -0200

    Category delete

--- src/main/java/com/java/services/CategoryService.java
+++ src/main/java/com/java/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.java.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.java.domain.Category;
 import com.java.repositories.CategoryRepository;
+import com.java.services.exceptions.DataIntegrityException;
 import com.java.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoryService {
 	public Category update(Category category) {
 		this.findById(category.getId());
 		return repository.save(category);
+	}
+
+	public void deleteById(Integer id) {
+		this.findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}		
 	}
 }
