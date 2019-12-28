50c80fadf0067c637d45e4f46ae4db945aedf4bc
FellipySaldanha <fellipy.saldanha@gmail.com>
2019-12-27 23:53:01 -0300

    Pagination with optional parameters

--- src/main/java/com/java/resources/CategoryResource.java
+++ src/main/java/com/java/resources/CategoryResource.java
@@ -4,18 +4,20 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.java.domain.Category;
+import com.java.dto.CategoryDTO;
+import com.java.services.CategoryService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.java.domain.Category;
-import com.java.dto.CategoryDTO;
-import com.java.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -54,6 +56,17 @@ public class CategoryResource {
     public ResponseEntity<List<CategoryDTO>> findAll(){
     	List<Category> categories = service.findAll();
     	List<CategoryDTO> listDtos = categories.stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
+    	return ResponseEntity.ok().body(listDtos);    			
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoryDTO>> findPage(
+        @RequestParam(value = "page", defaultValue = "0") Integer page, 
+        @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+        @RequestParam(value = "orderBy", defaultValue = "name") String orderBy, 
+        @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+        Page<Category> categories = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoryDTO> listDtos = categories.map(category -> new CategoryDTO(category));
     	return ResponseEntity.ok().body(listDtos);    			
     }
 }
--- src/main/java/com/java/services/CategoryService.java
+++ src/main/java/com/java/services/CategoryService.java
@@ -3,14 +3,17 @@ package com.java.services;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Service;
-
 import com.java.domain.Category;
 import com.java.repositories.CategoryRepository;
 import com.java.services.exceptions.DataIntegrityException;
 import com.java.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
@@ -45,5 +48,10 @@ public class CategoryService {
 
 	public List<Category> findAll() {
 		return repository.findAll();		
+	}
+
+	public Page<Category> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 }
