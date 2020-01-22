4029f1f5cc60e9a624ac1a54b3b85e47f929af75
fellipySaldanha <fellipy.saldanha@gmail.com>
2020-01-21 23:38:03 -0300

    Client retrieves his orders

--- src/main/java/com/java/resources/OrderResource.java
+++ src/main/java/com/java/resources/OrderResource.java
@@ -4,17 +4,19 @@ import java.net.URI;
 
 import javax.validation.Valid;
 
-import com.java.domain.Order;
-import com.java.services.OrderService;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.java.domain.Order;
+import com.java.services.OrderService;
 
 @RestController
 @RequestMapping(value = "/orders")
@@ -37,4 +39,13 @@ public class OrderResource {
     	return ResponseEntity.created(uri).build();
     }
     
+	@RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Page<Order>> findPage(
+        @RequestParam(value = "page", defaultValue = "0") Integer page, 
+        @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+        @RequestParam(value = "orderBy", defaultValue = "instant") String orderBy, 
+        @RequestParam(value = "direction", defaultValue = "DESC") String direction){
+        Page<Order> orders = service.findPage(page, linesPerPage, orderBy, direction);        
+    	return ResponseEntity.ok().body(orders);    			
+    }
 }
--- src/main/java/com/java/services/OrderService.java
+++ src/main/java/com/java/services/OrderService.java
@@ -3,6 +3,14 @@ package com.java.services;
 import java.util.Date;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.java.domain.Client;
 import com.java.domain.Order;
 import com.java.domain.OrderItem;
 import com.java.domain.PaymentSlip;
@@ -10,11 +18,9 @@ import com.java.domain.enums.PaymentStatus;
 import com.java.repositories.OrderItemRepository;
 import com.java.repositories.OrderRepository;
 import com.java.repositories.PaymentRepository;
+import com.java.security.UserSpringSecurity;
+import com.java.services.exceptions.AuthorizationException;
 import com.java.services.exceptions.ObjectNotFoundException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService {
@@ -70,6 +76,16 @@ public class OrderService {
 		orderItemRepository.saveAll(order.getItems());
 		emailService.sendOrderConfirmationHtmlEmail(order);
 		return order;
+	}
+	
+	public Page<Order> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSpringSecurity user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Client client = clientService.findById(user.getId());
+		return repository.findByClient(client, pageRequest);
 	}
 	
 }
