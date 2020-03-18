d263dc0e4f9fc473f6d117a39e7cf64bbb627ee0
fellipySaldanha <fellipy.saldanha@gmail.com>
2020-03-17 23:17:25 -0300

    File upload

--- src/main/java/com/java/JavaFullstackApplication.java
+++ src/main/java/com/java/JavaFullstackApplication.java
@@ -1,12 +1,26 @@
 package com.java;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.java.services.S3Service;
+
 @SpringBootApplication
-public class JavaFullstackApplication{
+public class JavaFullstackApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaFullstackApplication.class, args);
 	}		
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		s3Service.uploadFile("C:\\Users\\felli\\Documents\\teste.txt");
+		
+	}
 }
